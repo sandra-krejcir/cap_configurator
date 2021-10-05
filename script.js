9e720ca0c241808103aa563a76178ce29ce736ae
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-10-05 19:14:47 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,147 @@
+"use strict";
+
+// The model of all features
+const features = {
+  drinksholder: false,
+  led: false,
+  propeller: false,
+  shield: false,
+  solarfan: false,
+};
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  console.log("start");
+  // register toggle-clicks
+
+  document
+    .querySelectorAll(".option")
+    .forEach((option) => option.addEventListener("click", toggleOption));
+}
+
+function toggleOption(event) {
+  const target = event.currentTarget;
+  const feature = target.dataset.feature;
+  if (features[feature] === false) {
+    features[feature] = true;
+  } else {
+    features[feature] = false;
+  }
+
+  // TODO: Toggle feature in "model"
+
+  // If feature is (now) turned on:
+  // - mark target as chosen (add class "chosen")
+  // - un-hide the feature-layer(s) in the #product-preview;
+  // - create featureElement and append to #selected ul
+  // - create FLIP-animation to animate featureElement from img in target, to
+  //   its intended position. Do it with normal animation or transition class!
+
+  // Else - if the feature (became) turned off:
+  // - no longer mark target as chosen
+  // - hide the feature-layer(s) in the #product-preview
+  // - find the existing featureElement in #selected ul
+  // - create FLIP-animation to animate featureElement to img in target
+  // - when animation is complete, remove featureElement from the DOM
+
+  if (features[feature]) {
+    target.classList.add("chosen");
+    console.log(features[feature]);
+    document
+      .querySelector(`[data-feature='${feature}'`)
+      .classList.remove("hide");
+
+    let selectedFeature = createFeatureElement(feature);
+    document.querySelector("ul").appendChild(selectedFeature);
+
+    const firstFrame = document
+      .querySelector("#options")
+      .getBoundingClientRect();
+    console.log(firstFrame);
+
+    const lastFrame = selectedFeature.getBoundingClientRect();
+    console.log(lastFrame);
+
+    const deltaX = firstFrame.left - lastFrame.left;
+    const deltaY = firstFrame.top - lastFrame.top;
+    const deltaWidth = firstFrame.width / lastFrame.width;
+    const deltaHeight = firstFrame.height / lastFrame.height;
+
+    selectedFeature.animate(
+      [
+        {
+          transformOrigin: "top left",
+          transform: `translateX(${deltaX}px)
+      translateY(${deltaY}px) scaleX(${deltaWidth}) scaleY(${deltaHeight})`,
+        },
+        { transformOrigin: "top left", transform: "none" },
+      ],
+      {
+        duration: 300,
+        easing: "ease-in-out",
+      }
+    );
+
+    // feature added
+    console.log(`Feature ${feature} is turned on!`);
+
+    // TODO: More code
+  } else {
+    features[feature] = false;
+    // feature removed
+    target.classList.remove("chosen");
+    document.querySelector(`[data-feature='${feature}'`).classList.add("hide");
+    let theChild = document.querySelector(`li img alt="${feature}""`);
+
+    const firstFrame = document.querySelector(theChild).getBoundingClientRect();
+    console.log(firstFrame);
+
+    const lastFrame = document
+      .querySelector("#options")
+      .getBoundingClientRect();
+    console.log(lastFrame);
+
+    const deltaX = firstFrame.left - lastFrame.left;
+    const deltaY = firstFrame.top - lastFrame.top;
+    const deltaWidth = firstFrame.width / lastFrame.width;
+    const deltaHeight = firstFrame.height / lastFrame.height;
+
+    theChild.animate(
+      [
+        {
+          transformOrigin: "top left",
+          transform: `translateX(${deltaX}px)
+      translateY(${deltaY}px) scaleX(${deltaWidth}) scaleY(${deltaHeight})`,
+        },
+        { transformOrigin: "top left", transform: "none" },
+      ],
+      {
+        duration: 300,
+        easing: "ease-in-out",
+      }
+    );
+
+    console.log(`Feature ${feature} is turned off!`);
+
+    // TODO: More code
+  }
+}
+
+// Create featureElement to be appended to #selected ul - could have used a <template> instead
+function createFeatureElement(feature) {
+  const li = document.createElement("li");
+  li.dataset.feature = feature;
+
+  const img = document.createElement("img");
+  img.src = `images/feature_${feature}.png`;
+  img.alt = capitalize(feature);
+
+  li.append(img);
+
+  return li;
+}
+
+function capitalize(text) {
+  return text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase();
+}
